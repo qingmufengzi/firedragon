db3cc94fe5605df20a51815abd0d26ebad9b096b
jzy <1665496368@qq.com>
2019-10-30 11:04:53 +0800

    分模块第一次提交

---
+++ demo-parent/demo-web/src/main/java/com/demo/demoweb/DemoWebApplication.java
@@ -0,0 +1,20 @@
+package com.demo.demoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoWebApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoWebApplication.class);
+    }
+
+}
